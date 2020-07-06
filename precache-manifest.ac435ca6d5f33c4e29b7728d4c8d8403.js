aea27b5a93e03b82a02619bcd28fc4e4421e9ff3
JerryKaura <jerimkaura001@gmail.com>
2020-07-06 21:09:29 +0300

    Updates

---
+++ precache-manifest.ac435ca6d5f33c4e29b7728d4c8d8403.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c6706b9a5e24e4b1de636b44bc5eff5",
+    "url": "/adviceslipapi/index.html"
+  },
+  {
+    "revision": "56039d4d9a6048a62c4c",
+    "url": "/adviceslipapi/static/css/main.0c730f67.chunk.css"
+  },
+  {
+    "revision": "84099ef3c0c80770308f",
+    "url": "/adviceslipapi/static/js/2.6bb53ccd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/adviceslipapi/static/js/2.6bb53ccd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56039d4d9a6048a62c4c",
+    "url": "/adviceslipapi/static/js/main.4a31f7d3.chunk.js"
+  },
+  {
+    "revision": "8cd857ee09bb564e94d7",
+    "url": "/adviceslipapi/static/js/runtime-main.40e5e3b6.js"
+  },
+  {
+    "revision": "9daad6c578f5c99a40bed3821df03eda",
+    "url": "/adviceslipapi/static/media/nature-3082832_1920.9daad6c5.jpg"
+  }
+]);
